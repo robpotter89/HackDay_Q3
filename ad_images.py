da168c0e94691bef73bee17fca8e8dac2cb5c08a
RyanSammon <ryan.sammon@360pi.com>
2019-09-20 16:25:12 -0400

    Add debug mode.

--- ad_images.py
+++ ad_images.py
@@ -6,11 +6,11 @@ import base64
 from PIL import Image
 
 
-def get_ad_images_from_url(url):
+def get_ad_images_from_url(url, debug=False):
     ads = []
     chrome_options = Options()
     driver = webdriver.Chrome(options=chrome_options)
-    page_base64_image = ''
+    page_pil_image = None
     try:
         driver.get(url)
         sleep(5)
@@ -37,6 +37,11 @@ def get_ad_images_from_url(url):
                 driver.switch_to.default_content()
 
         page_base64_image = driver.get_screenshot_as_base64()
+        page_pil_image = Image.open(
+            BytesIO(base64.decodebytes(page_base64_image.encode()))
+        )
+        if debug:
+            page_pil_image.show()
 
     except Exception as e:
         print("exception", e)
@@ -45,21 +50,22 @@ def get_ad_images_from_url(url):
         driver.quit()
 
     ad_base64_images = []
-    for ad in ads:
-        location = ad[0]
-        size = ad[1]
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width']
-        bottom = location['y'] + size['height']
-        output = BytesIO()
-        pil_image = Image.open(
-            BytesIO(base64.decodebytes(page_base64_image.encode()))
-        )
-        pil_image = pil_image.crop((left, top, right, bottom))
-        pil_image.save(output, format='PNG')
-        output.seek(0)
-        ad_base64_image = base64.b64encode(output.read())
-        ad_base64_images.append(ad_base64_image)
+    if page_pil_image:
+        for ad in ads:
+            location = ad[0]
+            size = ad[1]
+            left = location['x']
+            top = location['y']
+            right = location['x'] + size['width']
+            bottom = location['y'] + size['height']
+            output = BytesIO()
+            ad_pil_image = page_pil_image.crop((left, top, right, bottom))
+            if debug:
+                ad_pil_image.show()
+
+            ad_pil_image.save(output, format='PNG')
+            output.seek(0)
+            ad_base64_image = base64.b64encode(output.read())
+            ad_base64_images.append(ad_base64_image)
 
     return ad_base64_images
